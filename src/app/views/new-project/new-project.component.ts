e384bc22015a250fdbf4d27515f1fda897fb4ead
PAULO EDUARDO FURTADO LOPES <pelopes@fgr.com.br>
2025-07-03 15:49:09 -0300

    feat: Cria pagina de upload de arquivo, todo: componentizar upload para reutilizacao

--- src/app/views/new-project/new-project.component.ts
+++ src/app/views/new-project/new-project.component.ts
@@ -1,13 +1,67 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {LayoutComponent} from '@components/layout/layout.component';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-new-project',
-  imports: [LayoutComponent],
+  imports: [LayoutComponent,CommonModule, MatIcon, MatIconModule, MatButtonModule ],
   templateUrl: './new-project.component.html',
   styleUrl: './new-project.component.scss'
 })
 export class NewProjectComponent {
+  constructor() {}
+
+  isDragOver = false;
+  fileName: string | null = null;
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      this.processFile(files[0]);
+    }
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.processFile(input.files[0]);
+    }
+  }
+
+  processFile(file: File) {
+    const validTypes = [
+      'application/vnd.ms-excel',
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    ];
+
+    const extensionValid = file.name.endsWith('.xls') || file.name.endsWith('.xlsx');
+    const mimeValid = validTypes.includes(file.type);
+
+    if (!extensionValid && !mimeValid) {
+      alert('Formato inválido. Apenas arquivos Excel são permitidos.');
+      return;
+    }
+
+    this.fileName = file.name;
+
+    // Aqui você pode enviar o arquivo para um service ou API
+    console.log('Arquivo recebido:', file);
+  }
 
 }
